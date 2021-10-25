a8c8a188c7ab0dfda464426726bbd49c6cdb3e0f
Vandana Muppavarapu <61150709+vandana002@users.noreply.github.com>
2021-10-25 18:39:39 +0530

    Update bus.tcl

--- bus.tcl
+++ bus.tcl
@@ -1,4 +1,5 @@
-et ns [new Simulator]
+set ns [new Simulator]
+
 set nf [open out.nam w]
 $ns namtrace-all $nf
 proc finish {} {
@@ -9,21 +10,18 @@ puts "running nam"
 exec nam out.nam &
 exit 0
 }
+
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
-$ns duplex-link $n0 $n1 10Mbps 10ms DropTail
-$ns duplex-link $n1 $n2 10Mbps 10ms DropTail
-$ns duplex-link $n2 $n3 10Mbps 10ms DropTail
-$ns duplex-link $n3 $n4 10Mbps 10ms DropTail
-$ns duplex-link $n4 $n5 10Mbps 10ms DropTail
-$ns duplex-link-op $n0 $n1 orient right
-$ns duplex-link-op $n1 $n2 orient right
-$ns duplex-link-op $n2 $n3 orient right
-$ns duplex-link-op $n3 $n4 orient right
-$ns duplex-link-op $n4 $n5 orient right
-$ns at 5.00 "finish"
+
+set lan [$ns newLan "$n0 $n1 $n2 $n3 $n4 $n5" 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
+
+
+
+
+$ns at 2.00 "finish"
 $ns run
